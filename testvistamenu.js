60da9d628d1d1fc6ba542a0978bc59f006a18984
Yoni Doron-Peters <jonathan.d@thoughtworks.com>
2024-01-19 08:58:03 -0700

    Update the vista menu script to run with vista-api-x

--- testvistamenu.js
+++ testvistamenu.js
@@ -4,31 +4,34 @@ var axios = require('axios');
 const fastcsv = require('fast-csv');
 const fs = require('fs');
 var parse = require('csv-parse');
-const config = require('./config');
 
 const cliProgress = require('cli-progress');
-const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+const bar1 = new cliProgress.SingleBar({hideCursor: true}, cliProgress.Presets.shades_classic);
 
-function getComInfo(stationNo,duz){
+// dev config
+// const config = require('./config.dev');
+// const accountsFile = "accounts-tests.csv"
 
-    return new Promise(function(resolve,reject){
+// prod config
+const config = require('./config');
+const accountsFile = "accounts.csv"
+
+function getComInfo(stationNo, duz) {
+
+  return new Promise(function (resolve, reject) {
     axios.post(config.vistaApi.tokenUrl,
-            {
-              "key":config.key,
-              "stationNo": stationNo,
-              "duz": duz
-                        }).then(function(data){
-          
-              axios.post(config.vistaApi.url,                
-                {
+      {
+        "key": config.key,
+      }).then(function (data) {
+        axios.post(config.vistaApi.url.replace('{stationNo}', stationNo).replace('{duz}', duz),
+          {
                   "context" : "OR CPRS GUI CHART",
                   "rpc" : "ORWU TOOLMENU", //ORWCOM GETOBJS
                   "jsonResult" : "false",
                   "parameters" : []
-                 },{  headers:{'authorization':'Bearer '+data.data.payload.token},}
-                ).then(function(data){
-           
-                  var jsonData = data.data;
+                }, { headers: { 'authorization': 'Bearer ' + data.data.data.token }, }                
+              ).then(function(data){
+                 var jsonData = data.data;
                  var resp = jsonData.payload
                  if (resp){
                
@@ -50,57 +53,68 @@ function getComInfo(stationNo,duz){
                     }
                   }
                })
+               .catch((err) => {
+                reject(err.response.data);
+               });
               })
-              .catch((err) => {
-                console.error(err);
+            //   .catch((err) => {
+            //     reject(err.response.data);
               
-            });
-      }) 
+            // });
+      })
   }
-function getStations(){
-    return new Promise(function(resolve,reject){
-                var stations = []
-                fs.createReadStream("accounts.csv")
-                    .pipe(parse.parse({delimiter: ','}))
-                    .on('data', function(csvrow) {
-                       var row = {
-                        "stationNo":csvrow[0],
-                        "accountDuz":csvrow[1]
-                       }
-                        stations.push(row)
-                    })
-                    .on('end', () => resolve(stations));
+function getStations() {
+    return new Promise(function (resolve, reject) {
+      var stations = []
+      fs.createReadStream(accountsFile)
+        .pipe(parse.parse({ delimiter: ',' }))
+        .on('data', function (csvrow) {
+          var row = {
+            "stationNo": csvrow[0],
+            "accountDuz": csvrow[1]
+          }
+          stations.push(row)
+        })
+        .on('end', () => resolve(stations));
     })
 }
 var comRes = []
 function addRes(comInfo){
-  comRes.push(...comInfo)
+  comRes.push(comInfo)
 }
 
-        const doConfig = async () => {
-          
-            try{
-                var stations = await getStations()
-                bar1.start(stations.length, 0);
-                for (var i=0;i<stations.length;i++){
-                    var comInfo = await getComInfo(stations[i].stationNo,stations[i].accountDuz)
+const doConfig = async () => {
 
-                    if(comInfo){
-                        addRes([comInfo])
-                         }
-                  bar1.increment()
-                }
-             }
-             catch (error){
-                 console.error(error)
-                 error_log(error)
-             }
-        const ws = fs.createWriteStream("results.csv");
-        fastcsv
-        .write(comRes, { headers: false })
-        .pipe(ws);
-         }
-        doConfig()
+  try {
+    var stations = await getStations()
+    // console.log("Stations: ", stations);
+    bar1.start(stations.length, 0);
+    for (var i = 0; i < stations.length; i++) {
+      try {
+        var comInfo = await getComInfo(stations[i].stationNo, stations[i].accountDuz);
+        console.log("comInfo: ", comInfo);
+        if (comInfo) {
+          addRes(comInfo);
+        } else {
+          console.log("No results");
+        }
+      } catch (err) {
+        console.error(err);
+      }
+      bar1.increment()
+    }
+  }
+  catch (error) {
+    console.error(error)
+    // error_log(error)
+  }
+  const ws = fs.createWriteStream("results_toolmenu.csv");
+  fastcsv
+    .write(comRes, { headers: false })
+    .pipe(ws);
+  bar1.stop();
+}
+doConfig()
 
     
 
